4176266b2b187bc8d87518e0cd422171546ee91c
[Centro1993] <[jonas.leitner93@gmail.com]>
2017-01-18 00:42:43 +0100

    changed pin setup

--- ledProcess.js
+++ ledProcess.js
@@ -16,7 +16,7 @@ pi.pinMode(rPin, pi.SOFT_PWM_OUTPUT);
 pi.pinMode(gPin, pi.SOFT_PWM_OUTPUT);
 pi.pinMode(bPin, pi.SOFT_PWM_OUTPUT);
 
-pi.softPwmCreate(rPin, 100, 100);
+console.log(pi.softPwmCreate(rPin, 100, 100));
 pi.softPwmCreate(gPin, 100, 100);
 pi.softPwmCreate(bPin, 100, 100);
 
