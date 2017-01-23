f08a323eaa89f44dcf961413f5e81b2acb63ee03
[Centro1993] <[jonas.leitner93@gmail.com]>
2017-01-23 20:39:22 +0100

    changed to softpwm

--- ledProcess.js
+++ ledProcess.js
@@ -11,23 +11,6 @@ const rPin = 2;
 const gPin = 3;
 const bPin = 4;
 
-pi.pinMode(rPin, pi.OUTPUT);
-pi.pinMode(gPin, pi.OUTPUT);
-pi.pinMode(bPin, pi.OUTPUT);
-
-pi.digitalWrite(rPin, pi.LOW);
-pi.digitalWrite(gPin, pi.LOW);
-pi.digitalWrite(bPin, pi.LOW);
-
-var intToDigital = function(value) {
-  if(value > 49) {
-    return pi.HIGH;
-  } else {
-    return pi.LOW;
-  }
-};
-
-/*
 pi.softPwmCreate(rPin, 100, 100);
 pi.softPwmCreate(gPin, 100, 100);
 pi.softPwmCreate(bPin, 100, 100);
@@ -35,7 +18,7 @@ pi.softPwmCreate(bPin, 100, 100);
 pi.softPwmWrite(rPin, r);
 pi.softPwmWrite(gPin, g);
 pi.softPwmWrite(bPin, b);
-*/
+
 
 //receive signal from parent
 process.on('message', function(msg) {
@@ -46,17 +29,10 @@ process.on('message', function(msg) {
     g = parseInt(msg.g);
     b = parseInt(msg.b);
 
-    //digital writes
-    pi.digitalWrite(rPin, intToDigital(r));
-    pi.digitalWrite(gPin, intToDigital(g));
-    pi.digitalWrite(bPin, intToDigital(b));
-
     //update pwm writes
-    /*
     pi.softPwmWrite(rPin, r);
     pi.softPwmWrite(gPin, g);
     pi.softPwmWrite(bPin, b);
-    */
 });
 
 //kill child process with parent
