e655c43e952e155d312a719df24d310e3e33ef29
[Centro1993] <[jonas.leitner93@gmail.com]>
2017-03-06 18:59:19 +0100

    added afk kick

--- server.js
+++ server.js
@@ -53,18 +53,18 @@ router.get('/', function(req, res) {
 });
 
 //slack webhook endpoint
-router.post('/slack', function (req, res) {
-  console.log(req.body);
-  led.send({
-      'function': 'blink',
-      'color': {
-        'r': 255,
-        'g': 1,
-        'b': 1
-      }
-  });
-  res.status(200);
-  res.end();
+router.post('/slack', function(req, res) {
+    console.log(req.body);
+    led.send({
+        'function': 'blink',
+        'color': {
+            'r': 255,
+            'g': 1,
+            'b': 1
+        }
+    });
+    res.status(200);
+    res.end();
 });
 
 app.use(router);
@@ -78,8 +78,10 @@ var g = 100;
 var b = 100;
 var controllingSocket = "";
 var controllingTimestamp = 0;
+var lastInputTimestamp = 0;
 var waitingSockets = [];
 const CONTROLTIME = 15000;
+const AFKTIME = 10000;
 
 //get time it takes for socket to become controller
 var waitingTimeCheck = function(socket) {
@@ -128,6 +130,9 @@ var controllerCheck = function() {
             control: true
         });
 
+        //initially set lastInputTimestamp
+        lastInputTimestamp = Date.now();
+
         //remove first socket from waiting list
         waitingSockets.splice(0, 1);
 
@@ -152,6 +157,35 @@ var controllerCheck = function() {
         io.to(controllingSocket).emit('controlling', {
             control: true
         });
+
+        //initially set lastInputTimestamp
+        lastInputTimestamp = Date.now();
+
+        //remove first socket from waiting list
+        waitingSockets.splice(0, 1);
+
+        return;
+    }
+
+    //replace controller and remove him from waitinglist if his lastInputTimestamp is older than AFKTIME
+    if (Date.now() - lastInputTimestamp > AFKTIME) {
+        //inform current controlling socket of controller change and afk status
+        io.to(controllingSocket).emit('controlling', {
+            control: false,
+            afk: true
+        });
+
+        //make first socket in waiting list the controller and set timestamp (thereby remove last controller from waiting list)
+        controllingSocket = waitingSockets[0];
+        controllingTimestamp = Date.now();
+
+        //inform new controlling socket
+        io.to(controllingSocket).emit('controlling', {
+            control: true
+        });
+
+        //initially set lastInputTimestamp
+        lastInputTimestamp = Date.now();
 
         //remove first socket from waiting list
         waitingSockets.splice(0, 1);
@@ -221,12 +255,16 @@ io.on('connection', function(socket) {
 
     //handle incoming values
     socket.on('color', function(msg) {
+
         //only process input from currently controlling socket
         if (socket.id === controllingSocket) {
+            //set lastInputTimestamp
+            lastInputTimestamp = Date.now();
+
             socket.broadcast.emit('color', {
-              'r': r,
-              'g': g,
-              'b': b
+                'r': r,
+                'g': g,
+                'b': b
             });
             //console.log('R Value: ' + msg);
             r = msg.r;
@@ -243,12 +281,18 @@ io.on('connection', function(socket) {
         }
     });
 
+    /*
     socket.on('function', function(msg) {
-      led.send({
-          'function': msg.function,
-          'color': msg.color
-      });
+        //set lastInputTimestamp
+        lastInputTimestamp = Date.now();
+
+        led.send({
+            'function': msg.function,
+            'color': msg.color
+        });
     });
+    */
+
 });
 
 // ====================
