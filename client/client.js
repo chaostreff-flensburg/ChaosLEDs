100fd5b5b8e153484d56aef6a61b31aa00a8c939
[Centro1993] <[jonas.leitner93@gmail.com]>
2017-03-08 23:22:30 +0100

    changed master url

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 var pi = require('wiring-pi'),
-    masterUrl = 'http://jonasleitner.de:80',
+    masterUrl = 'jonasleitner.de',
     socket = require('socket.io-client')(masterUrl);
 
 //join server as client
